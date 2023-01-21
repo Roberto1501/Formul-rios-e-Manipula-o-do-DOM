f7152ffb89e0816e2788c5b44d6a2a967c7305c1
Roberto Carvalho <robertocarvalho508@gmail.com>
2023-01-21 16:43:21 -0300

    on progress

---
+++ scrypt.js
@@ -0,0 +1,9 @@
+function armazenar(){
+    let nome = document.getElementById('nome').value
+    let endereco = document.getElementById('endereco').value
+    let cidade = document.getElementById('cidade').value
+    let estado = document.getElementById('estado').value
+    
+    let curriculo = document.getElementById('curriculo').value
+    console.log(nome,endereco,cidade,estado,radio,checkbox,curriculo)
+}
